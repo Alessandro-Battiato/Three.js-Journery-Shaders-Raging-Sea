6f5f2dcd1377c02583ef499961f57f1289e5b590
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-30 18:48:05 +0100

    refactor: code refactoring

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -2,7 +2,10 @@ uniform float uBigWavesElevation;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.y += sin(modelPosition.x) * uBigWavesElevation;
+
+    float elevation = sin(modelPosition.x) * uBigWavesElevation;
+
+    modelPosition.y += elevation;
     
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
