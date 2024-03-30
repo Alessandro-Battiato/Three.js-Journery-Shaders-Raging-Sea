2789aa8eaba25020771c4153a1b2e76d30ca4ad7
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-30 18:47:13 +0100

    feat: shaders rendering

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import waterVertexShader from './shaders/water/vertex.glsl';
+import waterFragmentShader from './shaders/water/fragment.glsl';
 
 /**
  * Base
@@ -21,7 +23,13 @@ const scene = new THREE.Scene()
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128)
 
 // Material
-const waterMaterial = new THREE.MeshBasicMaterial()
+const waterMaterial = new THREE.ShaderMaterial({
+    vertexShader: waterVertexShader,
+    fragmentShader: waterFragmentShader,
+    uniforms: {
+        uBigWavesElevation: { value: 0.2 }
+    }
+});
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
---
+++ src/shaders/water/fragment.glsl
@@ -0,0 +1,5 @@
+void main() {
+    #include <colorspace_fragment>;
+
+    gl_FragColor = vec4(0.5, 0.8, 1.0, 1.0);
+}
