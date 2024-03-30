5702d8d70784055ef00a0affe4c39724511e9238
alessandrobattiato <alessandrobattiato@creationdose.com>
2024-03-30 19:20:33 +0100

    feat: animated waves implemented

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -1,3 +1,4 @@
+uniform float uTime;
 uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
 
@@ -5,8 +6,8 @@ void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     float elevation =
-        sin(modelPosition.x * uBigWavesFrequency.x) *
-        sin(modelPosition.z * uBigWavesFrequency.y) *
+        sin(modelPosition.x * uBigWavesFrequency.x + uTime) *
+        sin(modelPosition.z * uBigWavesFrequency.y + uTime) *
         uBigWavesElevation;
 
     modelPosition.y += elevation;
